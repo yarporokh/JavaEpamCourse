3f4bd792c14ba955a8916aff5362e4508ba5dd29
yarporokh <yaroslavporohnenko@gmail.com>
2022-04-25 22:47:15 +0300

    Edit Iterators class

--- src/main/java/com/advanced/practice6/iterator/Iterators.java
+++ src/main/java/com/advanced/practice6/iterator/Iterators.java
@@ -1,25 +1,42 @@
 package com.advanced.practice6.iterator;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.IntStream;
 
 class Iterators {
 
-    public static Iterator<Integer> intArrayTwoTimesIterator(int[] array){
-        throw new UnsupportedOperationException();
+    public static Iterator<Integer> intArrayTwoTimesIterator(int[] array) {
+        return Arrays.stream(array).flatMap(value -> IntStream.of(value, value)).iterator();
     }
 
     public static Iterator<Integer> intArrayThreeTimesIterator(int[] array) {
-        throw new UnsupportedOperationException();
+        return Arrays.stream(array).flatMap(value -> IntStream.of(value, value, value)).iterator();
     }
 
     public static Iterator<Integer> intArrayFiveTimesIterator(int[] array) {
-        throw new UnsupportedOperationException();
-        }
-
-    public static Iterable<String> table(String[] columns, int[] rows){
-        throw new UnsupportedOperationException();
+        return Arrays.stream(array).flatMap(value -> IntStream.of(value, value, value, value, value)).iterator();
     }
 
+    public static Iterable<String> table(String[] columns, int[] rows) {
+        int len = columns.length * rows.length;
+        String[] arr = new String[len];
+        int column = 0;
+        int row = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (row % rows.length == 0 && row > 0) {
+                row = 0;
+                column++;
+            }
+            arr[i] = columns[column] + rows[row];
+            row++;
+        }
+
+        return List.of(arr);
+    }
 
 
 }

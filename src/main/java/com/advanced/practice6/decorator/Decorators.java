2438507f53b4df1cb23813124e6557f467a731e6
yarporokh <yaroslavporohnenko@gmail.com>
2022-04-26 12:26:38 +0300

    Edit Decorators class

--- src/main/java/com/advanced/practice6/decorator/Decorators.java
+++ src/main/java/com/advanced/practice6/decorator/Decorators.java
@@ -1,10 +1,18 @@
 package com.advanced.practice6.decorator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Decorators {
     public static List<String> evenIndexElementsSubList(List<String> sourceList) {
-        throw new UnsupportedOperationException();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            if (i % 2 == 0)
+                list.add(sourceList.get(i));
+        }
+
+        return list;
     }
 
 
